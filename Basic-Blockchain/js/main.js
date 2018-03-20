f0477c6b6052fea1646f44f352375fbb9b1b1716
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2018-03-20 19:05:06 +0530

    JS Blockchain Add Proof of Work

--- Basic-Blockchain/js/main.js
+++ Basic-Blockchain/js/main.js
@@ -2,7 +2,11 @@ const Block = require('./block.js');
 const Blockchain = require('./blockchain.js');
 
 let chatterjeeCoin = new Blockchain();
+
+console.log("MINING BLOCK 1:");
 chatterjeeCoin.addBlock(new Block(1, "11/03/2018", { amount: 4 }));
+
+console.log("MINING BLOCK 2:");
 chatterjeeCoin.addBlock(new Block(2, "18/03/2018", { amount: 10 }));
 
 console.log(JSON.stringify(chatterjeeCoin, null, 4));
